4fcd6beccf22c411c036a9bb5044e86db9d0cd8e
Ashish <98660699+a8ingale@users.noreply.github.com>
2022-12-24 21:04:07 -0500

    py script to stop ec2
    Stop instance thru python script using boto3.

---
+++ 3.StopEc2-python.py
@@ -0,0 +1,11 @@
+# How to stop instance thru python script using boto3. 
+
+import boto3
+
+ec2 = boto3.resource("ec2")
+
+ec2_instance = {"Name": "instance-type", "Values": ["t2.micro"]}
+ec2_tag = {"Name": "tag:Name", "Values": ["boto3-prod"]}
+
+for instance in ec2.instances.filter(Filters=[ec2_tag]):
+    instance.stop()

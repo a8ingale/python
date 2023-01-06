11ea7ab0951ca51ca5f0e704ff949ed33d9ea93d
Ashish <98660699+a8ingale@users.noreply.github.com>
2023-01-05 21:04:52 -0500

    flask commit
    Flask python sample

---
+++ 11.Flask-python-helloW.py
@@ -0,0 +1,15 @@
+#Py script using Flask for Hello world.
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return "Hello Python!"
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
